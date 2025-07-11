5481a3056ea09c80e7cf7a8ef66a3405adcba79a
zzzzzkkkl <3572540547@qq.com>
2025-07-11 16:49:43 +0800

    Add files via upload

---
+++ VariableDemo2.java
@@ -0,0 +1,16 @@
+public class VariableDemo2{
+    public static void main(String[] args){
+        //1.变量基本用法
+        int a=10;
+        System.out.println(a);
+
+        //2.变量参与运算
+        int c=10;
+        int b=20;
+        System.out.println(c+b);
+
+        //3.修改变量记录值
+        a=30;
+        System.out.println(a);
+    }
+}
