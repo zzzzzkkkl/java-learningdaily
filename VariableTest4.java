e934062c76ebf61a2bda6b83a183159df7b8b1df
zzzzzkkkl <3572540547@qq.com>
2025-07-17 18:03:18 +0800

    Add files via upload

---
+++ VariableTest4.java
@@ -0,0 +1,10 @@
+public class VariableTest4{
+    public static void main(String[] args){
+       //定义变量记录姓名
+       double price=5299.0;
+       String name="华为";
+       System.out.println(price);
+       System.out.println(name);
+
+    }
+}
