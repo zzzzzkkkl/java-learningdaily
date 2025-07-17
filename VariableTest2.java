862d9f6d8a54b8fecc56ea91b65b9ab2108069eb
zzzzzkkkl <3572540547@qq.com>
2025-07-17 18:01:45 +0800

    Add files via upload

---
+++ VariableTest2.java
@@ -0,0 +1,15 @@
+public class VariableTest2{
+    public static void main(String[] args){
+       //定义变量记录姓名
+        String name="黑马谢广坤";
+        int age=18;
+        char gender='男';
+        double height=180.1;
+        boolean flag=true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+        System.out.println(flag);
+    }
+}
