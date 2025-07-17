768f42cf4d873bca4f3b0873cb5e768cbeec7709
zzzzzkkkl <3572540547@qq.com>
2025-07-17 18:02:48 +0800

    Add files via upload

---
+++ VariableTest3.java
@@ -0,0 +1,14 @@
+public class VariableTest3{
+    public static void main(String[] args){
+       //定义变量记录姓名
+        String name="送出连回家";
+        String toster="刘鑫 张预提 高原";
+        int year=2020;
+        double grade=9.0;
+        System .out.println(name);
+        System.out.println(toster);
+        System.out.println(year);
+        System.out.println(grade);
+
+    }
+}
