e11e3388a83f51c23cd496d17c1ed6522ab8cee8
zzzzzkkkl <3572540547@qq.com>
2025-07-17 16:42:11 +0800

    Add files via upload

---
+++ VariableDemo3.java
@@ -0,0 +1,31 @@
+public class VariableDemo3{
+    public static void main(String[] args){
+        //byte
+        byte b=10;
+        System.out.println(b);
+        //short
+        short s=20;
+        System.out.println(s);
+        //int
+        int i=30;
+        System.out.println(i);
+        //long
+        long l=999999999;
+        System.out.println(l);
+
+        //float
+        float f=10.1F;
+        System.out.println(f);
+        //double
+        double d=20.2;
+        System.out.println(d);
+
+        //char
+        char c='中';
+        System.out.println(c);
+
+        //boolean,true/false
+        boolean o=true;
+        System.out.println(o);
+    }
+}
